2df8d55b00a7f2468010ff912ab51abe30753096
xuran03 <xuran03@baidu.com>
2021-02-18 17:15:59 +0800

    tab -> 4 space

--- parse.go
+++ parse.go
@@ -1,324 +1,324 @@
 package gophp
 
 import (
-	"fmt"
-	"regexp"
-	"strconv"
+    "fmt"
+    "regexp"
+    "strconv"
 )
 
 func Parse(source []byte) (interface{}, int, error) {
-	for i := 0; i < len(source); {
-		switch source[i] {
-		case 'b':
-			boolValue, length, err := parseBoolean(source[i:])
-			if err != nil {
-				return nil, 0, fmt.Errorf("%s at %d", err.Error(), i)
-			}
-			i += length
-			return boolValue, length, nil
-		case 'i':
-			intValue, length, err := parseInt(source[i:])
-			if err != nil {
-				return nil, 0, fmt.Errorf("%s at %d", err.Error(), i)
-			}
-			i += length
-			return intValue, length, nil
-		case 'd':
-			floatValue, length, err := parseFloat(source[i:])
-			if err != nil {
-				return nil, 0, fmt.Errorf("%s at %d", err.Error(), i)
-			}
-			i += length
-			return floatValue, length, nil
-		case 's':
-			stringValue, length, err := parseString(source[i:])
-			if err != nil {
-				return nil, 0, fmt.Errorf("%s at %d", err.Error(), i)
-			}
-			i += length
-			return stringValue, length, nil
-		case 'a':
-			arrayValue, length, err := parseArray(source[i:])
-			if err != nil {
-				return nil, 0, fmt.Errorf("%s at %d", err.Error(), i)
-			}
-			i += length
-			return arrayValue, length, nil
-		case 'N':
-			nilValue, length, err := parseNil(source[i:])
-			if err != nil {
-				return nil, 0, fmt.Errorf("%s at %d", err.Error(), i)
-			}
-			i += length
-			return nilValue, length, nil
-		case 'O':
-			objectValue, length, err := parseObject(source[i:])
-			if err != nil {
-				return nil, 0, fmt.Errorf("%s at %d", err.Error(), i)
-			}
-			i += length
-			return objectValue, length, nil
-		default:
-			return nil, 0, fmt.Errorf("unsupported type at %d", i)
-		}
-	}
-	return nil, 0, fmt.Errorf("invalid params")
+    for i := 0; i < len(source); {
+        switch source[i] {
+        case 'b':
+            boolValue, length, err := parseBoolean(source[i:])
+            if err != nil {
+                return nil, 0, fmt.Errorf("%s at %d", err.Error(), i)
+            }
+            i += length
+            return boolValue, length, nil
+        case 'i':
+            intValue, length, err := parseInt(source[i:])
+            if err != nil {
+                return nil, 0, fmt.Errorf("%s at %d", err.Error(), i)
+            }
+            i += length
+            return intValue, length, nil
+        case 'd':
+            floatValue, length, err := parseFloat(source[i:])
+            if err != nil {
+                return nil, 0, fmt.Errorf("%s at %d", err.Error(), i)
+            }
+            i += length
+            return floatValue, length, nil
+        case 's':
+            stringValue, length, err := parseString(source[i:])
+            if err != nil {
+                return nil, 0, fmt.Errorf("%s at %d", err.Error(), i)
+            }
+            i += length
+            return stringValue, length, nil
+        case 'a':
+            arrayValue, length, err := parseArray(source[i:])
+            if err != nil {
+                return nil, 0, fmt.Errorf("%s at %d", err.Error(), i)
+            }
+            i += length
+            return arrayValue, length, nil
+        case 'N':
+            nilValue, length, err := parseNil(source[i:])
+            if err != nil {
+                return nil, 0, fmt.Errorf("%s at %d", err.Error(), i)
+            }
+            i += length
+            return nilValue, length, nil
+        case 'O':
+            objectValue, length, err := parseObject(source[i:])
+            if err != nil {
+                return nil, 0, fmt.Errorf("%s at %d", err.Error(), i)
+            }
+            i += length
+            return objectValue, length, nil
+        default:
+            return nil, 0, fmt.Errorf("unsupported type at %d", i)
+        }
+    }
+    return nil, 0, fmt.Errorf("invalid params")
 }
 
 func parseBoolean(source []byte) (bool, int, error) {
-	if err := shouldBeginWith(source, 'b', 4); err != nil {
-		return false, 0, err
-	}
-	var value bool
-	switch source[2] {
-	case '1':
-		value = true
-	case '0':
-		value = false
-	default:
-		return false, 0, fmt.Errorf("should get '0' or '1' when parse bool")
-	}
-	if err := shouldEnd(source, 'b', 3); err != nil {
-		return false, 0, err
-	}
-	return value, 4, nil
+    if err := shouldBeginWith(source, 'b', 4); err != nil {
+        return false, 0, err
+    }
+    var value bool
+    switch source[2] {
+    case '1':
+        value = true
+    case '0':
+        value = false
+    default:
+        return false, 0, fmt.Errorf("should get '0' or '1' when parse bool")
+    }
+    if err := shouldEnd(source, 'b', 3); err != nil {
+        return false, 0, err
+    }
+    return value, 4, nil
 }
 
 func parseInt(source []byte) (int64, int, error) {
-	if err := shouldBeginWith(source, 'i', 4); err != nil {
-		return 0, 0, err
-	}
-	i := 2
-	for ; source[i] >= '0' && source[i] <= '9' || source[i] == '-'; i++ {
-		//nothing here
-	}
-	if err := shouldEnd(source, 'i', i); err != nil {
-		return 0, 0, err
-	}
-	stringValue := string(source[2:i])
-	exp := regexp.MustCompile(`^[-+]?\d+$`)
-	if !exp.MatchString(stringValue) {
-		return 0, 0, fmt.Errorf("invalid int value: %s", stringValue)
-	}
-	value, _ := strconv.ParseInt(stringValue, 10, 64) //ignore error
-	return value, i + 1, nil
+    if err := shouldBeginWith(source, 'i', 4); err != nil {
+        return 0, 0, err
+    }
+    i := 2
+    for ; source[i] >= '0' && source[i] <= '9' || source[i] == '-'; i++ {
+        //nothing here
+    }
+    if err := shouldEnd(source, 'i', i); err != nil {
+        return 0, 0, err
+    }
+    stringValue := string(source[2:i])
+    exp := regexp.MustCompile(`^[-+]?\d+$`)
+    if !exp.MatchString(stringValue) {
+        return 0, 0, fmt.Errorf("invalid int value: %s", stringValue)
+    }
+    value, _ := strconv.ParseInt(stringValue, 10, 64) //ignore error
+    return value, i + 1, nil
 }
 
 func parseFloat(source []byte) (float64, int, error) {
-	if err := shouldBeginWith(source, 'd', 5); err != nil {
-		return 0, 0, err
-	}
-	i := 2
-	for ; source[i] >= '0' && source[i] <= '9' || source[i] == '-' || source[i] == '.'; i++ {
-		//nothing here
-	}
-	if err := shouldEnd(source, 'd', i); err != nil {
-		return 0, 0, err
-	}
-	stringValue := string(source[2:i])
-	exp := regexp.MustCompile(`^[-+]?\d+(\.\d*)?$`)
-	if !exp.MatchString(stringValue) {
-		return 0, 0, fmt.Errorf("invalid float value: %s", stringValue)
-	}
-	value, _ := strconv.ParseFloat(stringValue, 64)
-	return value, i + 1, nil
+    if err := shouldBeginWith(source, 'd', 5); err != nil {
+        return 0, 0, err
+    }
+    i := 2
+    for ; source[i] >= '0' && source[i] <= '9' || source[i] == '-' || source[i] == '.'; i++ {
+        //nothing here
+    }
+    if err := shouldEnd(source, 'd', i); err != nil {
+        return 0, 0, err
+    }
+    stringValue := string(source[2:i])
+    exp := regexp.MustCompile(`^[-+]?\d+(\.\d*)?$`)
+    if !exp.MatchString(stringValue) {
+        return 0, 0, fmt.Errorf("invalid float value: %s", stringValue)
+    }
+    value, _ := strconv.ParseFloat(stringValue, 64)
+    return value, i + 1, nil
 }
 
 func parseString(source []byte) (string, int, error) {
-	if err := shouldBeginWith(source, 's', 8); err != nil {
-		return "", 0, err
-	}
-	i := 2
-	//获取string长度
-	for ; source[i] >= '0' && source[i] <= '9'; i++ {
-		//nothing
-	}
-	stringLength, _ := strconv.ParseInt(string(source[2:i]), 10, 64)
-	if source[i] != ':' {
-		return "", 0, fmt.Errorf("should get ':' when parse string")
-	}
-	i++
-	if source[i] != '"' {
-		return "", 0, fmt.Errorf("should get '\"' when parse string")
-	}
-	if len(source) <= i+int(stringLength)+2 { //后面还有";
-		return "", 0, fmt.Errorf("invalid string length")
-	}
-	i++
-	value := string(source[i : i+int(stringLength)])
-	i += int(stringLength)
-	if source[i] != '"' {
-		return "", 0, fmt.Errorf("should get '\"' when parse string")
-	}
-	i++
-	if err := shouldEnd(source, 's', i); err != nil {
-		return "", 0, err
-	}
-	return value, i + 1, nil
+    if err := shouldBeginWith(source, 's', 8); err != nil {
+        return "", 0, err
+    }
+    i := 2
+    //获取string长度
+    for ; source[i] >= '0' && source[i] <= '9'; i++ {
+        //nothing
+    }
+    stringLength, _ := strconv.ParseInt(string(source[2:i]), 10, 64)
+    if source[i] != ':' {
+        return "", 0, fmt.Errorf("should get ':' when parse string")
+    }
+    i++
+    if source[i] != '"' {
+        return "", 0, fmt.Errorf("should get '\"' when parse string")
+    }
+    if len(source) <= i+int(stringLength)+2 { //后面还有";
+        return "", 0, fmt.Errorf("invalid string length")
+    }
+    i++
+    value := string(source[i : i+int(stringLength)])
+    i += int(stringLength)
+    if source[i] != '"' {
+        return "", 0, fmt.Errorf("should get '\"' when parse string")
+    }
+    i++
+    if err := shouldEnd(source, 's', i); err != nil {
+        return "", 0, err
+    }
+    return value, i + 1, nil
 }
 
 //parseArray 可能返回切片或者map
 //因为php的数组是可以混合的类型
 func parseArray(source []byte) (interface{}, int, error) {
-	if err := shouldBeginWith(source, 'a', 6); err != nil {
-		return nil, 0, err
-	}
-	i := 2
-	//获取array长度
-	for ; source[i] >= '0' && source[i] <= '9'; i++ {
-		//nothing
-	}
-	arrayLength, _ := strconv.ParseInt(string(source[2:i]), 10, 64)
-	if source[i] != ':' {
-		return nil, 0, fmt.Errorf("should get ':' when parse array")
-	}
-	i++
-	if source[i] != '{' {
-		return nil, 0, fmt.Errorf("should get '{' when parse array")
-	}
-	i++
-	isPureArray := true //如果是纯净的数组，返回切片
-	lastIntIndex := int64(-1)
-	arrayValue := make([]interface{}, 0, arrayLength)  //切片返回值
-	value := make(map[string]interface{}, arrayLength) //map返回值
-	for index := 0; index < int(arrayLength); index++ {
-		switch source[i] {
-		case 'i':
-			intKey, length, err := parseInt(source[i:])
-			if err != nil {
-				return nil, 0, err
-			}
-			i += length
-			curValue, length, err := Parse(source[i:])
-			if err != nil {
-				return nil, 0, err
-			}
-			i += length
-			value[fmt.Sprint(intKey)] = curValue
-			if intKey == lastIntIndex+1 {
-				lastIntIndex = intKey
-				arrayValue = append(arrayValue, curValue)
-			} else {
-				isPureArray = false
-			}
-		case 's':
-			stringKey, length, err := parseString(source[i:])
-			if err != nil {
-				return nil, 0, err
-			}
-			i += length
-			curValue, length, err := Parse(source[i:])
-			if err != nil {
-				return nil, 0, err
-			}
-			i += length
-			value[stringKey] = curValue
-			isPureArray = false
-		default:
-			return nil, 0, fmt.Errorf("unsupported array key")
-		}
-	}
-	if source[i] != '}' {
-		return nil, 0, fmt.Errorf("should get '}' when parse array")
-	}
-	if isPureArray {
-		return arrayValue, i + 1, nil
-	}
-	return value, i + 1, nil
+    if err := shouldBeginWith(source, 'a', 6); err != nil {
+        return nil, 0, err
+    }
+    i := 2
+    //获取array长度
+    for ; source[i] >= '0' && source[i] <= '9'; i++ {
+        //nothing
+    }
+    arrayLength, _ := strconv.ParseInt(string(source[2:i]), 10, 64)
+    if source[i] != ':' {
+        return nil, 0, fmt.Errorf("should get ':' when parse array")
+    }
+    i++
+    if source[i] != '{' {
+        return nil, 0, fmt.Errorf("should get '{' when parse array")
+    }
+    i++
+    isPureArray := true //如果是纯净的数组，返回切片
+    lastIntIndex := int64(-1)
+    arrayValue := make([]interface{}, 0, arrayLength)  //切片返回值
+    value := make(map[string]interface{}, arrayLength) //map返回值
+    for index := 0; index < int(arrayLength); index++ {
+        switch source[i] {
+        case 'i':
+            intKey, length, err := parseInt(source[i:])
+            if err != nil {
+                return nil, 0, err
+            }
+            i += length
+            curValue, length, err := Parse(source[i:])
+            if err != nil {
+                return nil, 0, err
+            }
+            i += length
+            value[fmt.Sprint(intKey)] = curValue
+            if intKey == lastIntIndex+1 {
+                lastIntIndex = intKey
+                arrayValue = append(arrayValue, curValue)
+            } else {
+                isPureArray = false
+            }
+        case 's':
+            stringKey, length, err := parseString(source[i:])
+            if err != nil {
+                return nil, 0, err
+            }
+            i += length
+            curValue, length, err := Parse(source[i:])
+            if err != nil {
+                return nil, 0, err
+            }
+            i += length
+            value[stringKey] = curValue
+            isPureArray = false
+        default:
+            return nil, 0, fmt.Errorf("unsupported array key")
+        }
+    }
+    if source[i] != '}' {
+        return nil, 0, fmt.Errorf("should get '}' when parse array")
+    }
+    if isPureArray {
+        return arrayValue, i + 1, nil
+    }
+    return value, i + 1, nil
 }
 
 func parseNil(source []byte) (interface{}, int, error) {
-	if len(source) < 2 || source[0] != 'N' || source[1] != ';' {
-		return nil, 0, fmt.Errorf("invalid nil value")
-	}
-	return nil, 2, nil
+    if len(source) < 2 || source[0] != 'N' || source[1] != ';' {
+        return nil, 0, fmt.Errorf("invalid nil value")
+    }
+    return nil, 2, nil
 }
 
 func parseObject(source []byte) (map[string]interface{}, int, error) {
-	if err := shouldBeginWith(source, 'O', 12); err != nil {
-		return nil, 0, err
-	}
-	i := 2
-	//获取class name长度
-	for ; source[i] >= '0' && source[i] <= '9'; i++ {
-		//nothing
-	}
-	classNameLength, _ := strconv.ParseInt(string(source[2:i]), 10, 64)
-	if source[i] != ':' {
-		return nil, 0, fmt.Errorf("should get ':' when parse object")
-	}
-	i++
-	if source[i] != '"' {
-		return nil, 0, fmt.Errorf("should get '\"' when parse object")
-	}
-	if len(source) <= i+int(classNameLength)+6 { //后面还有":0:{}
-		return nil, 0, fmt.Errorf("invalid class name length")
-	}
-	i++
-	i += int(classNameLength) //ignore class name
-	if source[i] != '"' {
-		return nil, 0, fmt.Errorf("should get '\"' when parse object")
-	}
-	i++
-	if source[i] != ':' {
-		return nil, 0, fmt.Errorf("should get ':' when parse object")
-	}
-	i++
-	curIndex := i
-	//获取object长度
-	for ; source[i] >= '0' && source[i] <= '9'; i++ {
-		//nothing
-	}
-	objectLength, _ := strconv.ParseInt(string(source[curIndex:i]), 10, 64)
-	if source[i] != ':' {
-		return nil, 0, fmt.Errorf("should get ':' when parse array")
-	}
-	i++
-	if source[i] != '{' {
-		return nil, 0, fmt.Errorf("should get '{' when parse array")
-	}
-	i++
-	value := make(map[string]interface{}, objectLength)
-	for index := 0; index < int(objectLength); index++ {
-		switch source[i] {
-		case 's':
-			stringKey, length, err := parseString(source[i:])
-			if err != nil {
-				return nil, 0, err
-			}
-			i += length
-			curValue, length, err := Parse(source[i:])
-			if err != nil {
-				return nil, 0, err
-			}
-			i += length
-			value[stringKey] = curValue
-		default:
-			return nil, 0, fmt.Errorf("unsupported object key")
-		}
-	}
-	if source[i] != '}' {
-		return nil, 0, fmt.Errorf("should get '}' when parse object")
-	}
-	return value, i + 1, nil
+    if err := shouldBeginWith(source, 'O', 12); err != nil {
+        return nil, 0, err
+    }
+    i := 2
+    //获取class name长度
+    for ; source[i] >= '0' && source[i] <= '9'; i++ {
+        //nothing
+    }
+    classNameLength, _ := strconv.ParseInt(string(source[2:i]), 10, 64)
+    if source[i] != ':' {
+        return nil, 0, fmt.Errorf("should get ':' when parse object")
+    }
+    i++
+    if source[i] != '"' {
+        return nil, 0, fmt.Errorf("should get '\"' when parse object")
+    }
+    if len(source) <= i+int(classNameLength)+6 { //后面还有":0:{}
+        return nil, 0, fmt.Errorf("invalid class name length")
+    }
+    i++
+    i += int(classNameLength) //ignore class name
+    if source[i] != '"' {
+        return nil, 0, fmt.Errorf("should get '\"' when parse object")
+    }
+    i++
+    if source[i] != ':' {
+        return nil, 0, fmt.Errorf("should get ':' when parse object")
+    }
+    i++
+    curIndex := i
+    //获取object长度
+    for ; source[i] >= '0' && source[i] <= '9'; i++ {
+        //nothing
+    }
+    objectLength, _ := strconv.ParseInt(string(source[curIndex:i]), 10, 64)
+    if source[i] != ':' {
+        return nil, 0, fmt.Errorf("should get ':' when parse array")
+    }
+    i++
+    if source[i] != '{' {
+        return nil, 0, fmt.Errorf("should get '{' when parse array")
+    }
+    i++
+    value := make(map[string]interface{}, objectLength)
+    for index := 0; index < int(objectLength); index++ {
+        switch source[i] {
+        case 's':
+            stringKey, length, err := parseString(source[i:])
+            if err != nil {
+                return nil, 0, err
+            }
+            i += length
+            curValue, length, err := Parse(source[i:])
+            if err != nil {
+                return nil, 0, err
+            }
+            i += length
+            value[stringKey] = curValue
+        default:
+            return nil, 0, fmt.Errorf("unsupported object key")
+        }
+    }
+    if source[i] != '}' {
+        return nil, 0, fmt.Errorf("should get '}' when parse object")
+    }
+    return value, i + 1, nil
 }
 
 func shouldBeginWith(source []byte, t byte, minLen int) error {
-	if len(source) < minLen {
-		return fmt.Errorf("invalid int length")
-	}
-	if source[0] != t {
-		return fmt.Errorf("should get '%c' when parse bool", t)
-	}
-	if source[1] != ':' {
-		return fmt.Errorf("should have ':' when parse '%c'", t)
-	}
-	return nil
+    if len(source) < minLen {
+        return fmt.Errorf("invalid int length")
+    }
+    if source[0] != t {
+        return fmt.Errorf("should get '%c' when parse bool", t)
+    }
+    if source[1] != ':' {
+        return fmt.Errorf("should have ':' when parse '%c'", t)
+    }
+    return nil
 }
 
 func shouldEnd(source []byte, t byte, index int) error {
-	if len(source) <= index || source[index] != ';' {
-		return fmt.Errorf("not end with ';' when parse '%c'", t)
-	}
-	return nil
+    if len(source) <= index || source[index] != ';' {
+        return fmt.Errorf("not end with ';' when parse '%c'", t)
+    }
+    return nil
 }
--- parse_test.go
+++ parse_test.go
@@ -1,116 +1,116 @@
 package gophp
 
 import (
-	"fmt"
-	"math"
-	"testing"
+    "fmt"
+    "math"
+    "testing"
 )
 
 func TestParse(t *testing.T) {
-	testCases := []string{
-		`b:1;`,
-		`b:0;`,
-		`i:123;`,
-		`d:3.1415;`,
-		`s:43:"a ba a ba, "(){}?:"><?!@#$%^&*-=_+|'\中国";`,
-		`N;`,
-		`O:3:"cls":1:{s:5:"prop1";s:3:"ppp";}`,
-		`a:4:{i:0;b:1;i:1;b:0;i:2;i:123;i:3;d:3.1415;}`,
-		`a:7:{i:0;b:1;i:3;b:0;i:4;i:123;i:5;d:3.1415;s:3:"str";s:43:"a ba a ba, "(){}?:"><?!@#$%^&*-=_+|'\中国";s:4:"null";N;s:3:"obj";O:3:"cls":1:{s:5:"prop1";s:3:"ppp";}}`,
-	}
+    testCases := []string{
+        `b:1;`,
+        `b:0;`,
+        `i:123;`,
+        `d:3.1415;`,
+        `s:43:"a ba a ba, "(){}?:"><?!@#$%^&*-=_+|'\中国";`,
+        `N;`,
+        `O:3:"cls":1:{s:5:"prop1";s:3:"ppp";}`,
+        `a:4:{i:0;b:1;i:1;b:0;i:2;i:123;i:3;d:3.1415;}`,
+        `a:7:{i:0;b:1;i:3;b:0;i:4;i:123;i:5;d:3.1415;s:3:"str";s:43:"a ba a ba, "(){}?:"><?!@#$%^&*-=_+|'\中国";s:4:"null";N;s:3:"obj";O:3:"cls":1:{s:5:"prop1";s:3:"ppp";}}`,
+    }
 
-	boolVal, _, err := Parse([]byte(testCases[0]))
-	if err != nil {
-		t.Errorf("parse %s got error: %v", testCases[0], err)
-	}
-	boolVal1 := boolVal.(bool)
-	if boolVal1 != true {
-		t.Errorf("parse %s got %v", testCases[0], boolVal1)
-	}
+    boolVal, _, err := Parse([]byte(testCases[0]))
+    if err != nil {
+        t.Errorf("parse %s got error: %v", testCases[0], err)
+    }
+    boolVal1 := boolVal.(bool)
+    if boolVal1 != true {
+        t.Errorf("parse %s got %v", testCases[0], boolVal1)
+    }
 
-	boolVal, _, err = Parse([]byte(testCases[1]))
-	if err != nil {
-		t.Errorf("parse %s got error: %v", testCases[1], err)
-	}
-	boolVal0 := boolVal.(bool)
-	if boolVal0 != false {
-		t.Errorf("parse %s got %v", testCases[1], boolVal0)
-	}
+    boolVal, _, err = Parse([]byte(testCases[1]))
+    if err != nil {
+        t.Errorf("parse %s got error: %v", testCases[1], err)
+    }
+    boolVal0 := boolVal.(bool)
+    if boolVal0 != false {
+        t.Errorf("parse %s got %v", testCases[1], boolVal0)
+    }
 
-	intVal, _, err := Parse([]byte(testCases[2]))
-	if err != nil {
-		t.Errorf("parse %s got error: %v", testCases[2], err)
-	}
-	int64Val := intVal.(int64)
-	if int64Val != 123 {
-		t.Errorf("parse %s got (%[2]T) %[2]v", testCases[2], int64Val)
-	}
+    intVal, _, err := Parse([]byte(testCases[2]))
+    if err != nil {
+        t.Errorf("parse %s got error: %v", testCases[2], err)
+    }
+    int64Val := intVal.(int64)
+    if int64Val != 123 {
+        t.Errorf("parse %s got (%[2]T) %[2]v", testCases[2], int64Val)
+    }
 
-	floatVal, _, err := Parse([]byte(testCases[3]))
-	if err != nil {
-		t.Errorf("parse %s got error: %v", testCases[3], err)
-	}
-	float64Val := floatVal.(float64)
-	//浮点数比较
-	if math.Abs(float64Val - 3.1415) > 0.0001 {
-		t.Errorf("parse %s got %v", testCases[3], float64Val)
-	}
+    floatVal, _, err := Parse([]byte(testCases[3]))
+    if err != nil {
+        t.Errorf("parse %s got error: %v", testCases[3], err)
+    }
+    float64Val := floatVal.(float64)
+    //浮点数比较
+    if math.Abs(float64Val - 3.1415) > 0.0001 {
+        t.Errorf("parse %s got %v", testCases[3], float64Val)
+    }
 
-	stringVal, _, err := Parse([]byte(testCases[4]))
-	if err != nil {
-		t.Errorf("parse %s got error: %v", testCases[4], err)
-	}
-	stringStrVal := stringVal.(string)
-	if stringStrVal != `a ba a ba, "(){}?:"><?!@#$%^&*-=_+|'\中国` {
-		t.Errorf("parse %s got %v", testCases[4], stringStrVal)
-	}
+    stringVal, _, err := Parse([]byte(testCases[4]))
+    if err != nil {
+        t.Errorf("parse %s got error: %v", testCases[4], err)
+    }
+    stringStrVal := stringVal.(string)
+    if stringStrVal != `a ba a ba, "(){}?:"><?!@#$%^&*-=_+|'\中国` {
+        t.Errorf("parse %s got %v", testCases[4], stringStrVal)
+    }
 
-	nilVal, _, err := Parse([]byte(testCases[5]))
-	if err != nil {
-		t.Errorf("parse %s got error: %v", testCases[5], err)
-	}
-	if nilVal != nil {
-		t.Errorf("parse %s got (%[2]T) %[2]v", testCases[5], nilVal)
-	}
+    nilVal, _, err := Parse([]byte(testCases[5]))
+    if err != nil {
+        t.Errorf("parse %s got error: %v", testCases[5], err)
+    }
+    if nilVal != nil {
+        t.Errorf("parse %s got (%[2]T) %[2]v", testCases[5], nilVal)
+    }
 
-	objectVal, _, err := Parse([]byte(testCases[6]))
-	if err != nil {
-		t.Errorf("parse %s got error: %v", testCases[6], err)
-	}
-	objectObjVal := objectVal.(map[string]interface{})
-	prop, ok := objectObjVal["prop1"]
-	if !ok {
-		t.Errorf("parse %s got no property: %v", testCases[6], "prop1")
-	} else if propStr := prop.(string); propStr != "ppp" {
-		t.Errorf("parse %s got property: %v value: %v", testCases[6], "prop1", propStr)
-	}
+    objectVal, _, err := Parse([]byte(testCases[6]))
+    if err != nil {
+        t.Errorf("parse %s got error: %v", testCases[6], err)
+    }
+    objectObjVal := objectVal.(map[string]interface{})
+    prop, ok := objectObjVal["prop1"]
+    if !ok {
+        t.Errorf("parse %s got no property: %v", testCases[6], "prop1")
+    } else if propStr := prop.(string); propStr != "ppp" {
+        t.Errorf("parse %s got property: %v value: %v", testCases[6], "prop1", propStr)
+    }
 
-	arrayVal, _, err := Parse([]byte(testCases[7]))
-	if err != nil {
-		t.Errorf("parse %s got error: %v", testCases[7], err)
-	}
-	arraySliceVal := arrayVal.([]interface{})
-	if len(arraySliceVal) != 4 {
-		t.Errorf("parse %s got array length: %d", testCases[7], len(arraySliceVal))
-	}
-	if item3 := arraySliceVal[2].(int64); item3 != 123 {
-		t.Errorf("parse %s got item3: %d", testCases[7], item3)
-	}
+    arrayVal, _, err := Parse([]byte(testCases[7]))
+    if err != nil {
+        t.Errorf("parse %s got error: %v", testCases[7], err)
+    }
+    arraySliceVal := arrayVal.([]interface{})
+    if len(arraySliceVal) != 4 {
+        t.Errorf("parse %s got array length: %d", testCases[7], len(arraySliceVal))
+    }
+    if item3 := arraySliceVal[2].(int64); item3 != 123 {
+        t.Errorf("parse %s got item3: %d", testCases[7], item3)
+    }
 
-	fmt.Println(arraySliceVal)
+    fmt.Println(arraySliceVal)
 
-	mapVal, _, err := Parse([]byte(testCases[8]))
-	if err != nil {
-		t.Errorf("parse %s got error: %v", testCases[8], err)
-	}
-	mapMVal := mapVal.(map[string]interface{})
-	if len(mapMVal) != 7 {
-		t.Errorf("parse %s got map length: %d", testCases[8], len(mapMVal))
-	} else if itemStr, ok := mapMVal["str"]; !ok {
-		t.Errorf("parse %s got no value for key: %s", testCases[8], "str")
-	} else if itemString := itemStr.(string); itemString != `a ba a ba, "(){}?:"><?!@#$%^&*-=_+|'\中国` {
-		t.Errorf("parse %s got %s value %v", testCases[8], "str", itemString)
-	}
+    mapVal, _, err := Parse([]byte(testCases[8]))
+    if err != nil {
+        t.Errorf("parse %s got error: %v", testCases[8], err)
+    }
+    mapMVal := mapVal.(map[string]interface{})
+    if len(mapMVal) != 7 {
+        t.Errorf("parse %s got map length: %d", testCases[8], len(mapMVal))
+    } else if itemStr, ok := mapMVal["str"]; !ok {
+        t.Errorf("parse %s got no value for key: %s", testCases[8], "str")
+    } else if itemString := itemStr.(string); itemString != `a ba a ba, "(){}?:"><?!@#$%^&*-=_+|'\中国` {
+        t.Errorf("parse %s got %s value %v", testCases[8], "str", itemString)
+    }
 
-	fmt.Println(mapMVal)
+    fmt.Println(mapMVal)
 }
